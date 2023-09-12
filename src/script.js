7d1609550fdcb6e2b531f35da1355bd4ce1f6031
AlexHerbertGit <alex.herbert.tech@gmail.com>
2023-09-12 12:25:21 +1200

    Did some more event handlers and event listeners

--- src/script.js
+++ src/script.js
@@ -1,13 +1,19 @@
 // NOTE
 
-let btn = document.getElementById('btnRadio')
-let output = document.getElementById('output')
+// let btn = document.getElementById('btnRadio')
+// let output = document.getElementById('output')
 
+// btn.addEventListener('click', () => {
+    // let languages = document.getElementsByName('language')
+        // languages.forEach((language) => {
+            // if(language.checked) {
+                // output.innerText = `You code with : ${language.value}`
+            // }
+        // })
+// });
+
+let btn = document.getElementById('btnCount')
 btn.addEventListener('click', () => {
-    let languages = document.getElementsByName('language')
-        languages.forEach((language) => {
-            if(language.checked) {
-                output.innerText = `You code with : ${language.value}`
-            }
-        })
-});
+    let hTwoHeader = document.getElementsByTagName('h2')
+    alert(`The number of H2 tags are: ${hTwoHeader.length}`) 
+})
